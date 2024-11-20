385c4119c3b30ddf8b2b1eee52ee1be1a04fb85a
Ezequiel <ezee7771@gmail.com>
2024-11-20 19:31:49 -0300

    arreglo bd dialect

--- Documents/JERFFRE - Personal/ORT/Cuatrimestre 4 - 2024/TP2 - Taller de Programación 2/TP2-TPFINAL/config/config.js
+++ Documents/JERFFRE - Personal/ORT/Cuatrimestre 4 - 2024/TP2 - Taller de Programación 2/TP2-TPFINAL/config/config.js
@@ -1,5 +1,17 @@
 // config.js
 
+const SERVER_PORT = 8080
+const DB_NAME = "TP2_TPFINAL"
+const DB_USER = "eze"
+const DB_PASS = "eze1234"
+const DB_HOST = "localhost"
+const DB_DIALECT = "mssql"
+const DB_PORT = 1433
+
+
+export { SERVER_PORT, DB_NAME, DB_USER, DB_PASS, DB_HOST, DB_DIALECT, DB_PORT };
+
+/*
 const SERVER_PORT = process.env.SERVER_PORT;
 const DB_NAME = process.env.DB_NAME;
 const DB_USER = process.env.DB_USER;
@@ -7,6 +19,4 @@ const DB_PASS = process.env.DB_PASS;
 const DB_HOST = process.env.DB_HOST;
 const DB_DIALECT = process.env.DB_DIALECT; //SQL SERVER
 const DB_PORT = process.env.DB_PORT;
-
-
-export { SERVER_PORT, DB_NAME, DB_USER, DB_PASS, DB_HOST, DB_DIALECT, DB_PORT };
+*/
